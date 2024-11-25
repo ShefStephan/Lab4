234b4a433b1da96a9a390b32a60c18fada0a0777
Stephan <kochurov05@mail.ru>
2024-11-25 18:49:42 +0300

    add DataTemplate aand Styles for TextBlock and Button

--- TurtleWPF/AppViewModel/ViewModel.cs
+++ TurtleWPF/AppViewModel/ViewModel.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using TurtleWPF.CommandOperations;
 using TurtleWPF.TurtleCommands;
 using TurtleWPF.DataBase;
-using System.Reflection.PortableExecutable;
-using System.DirectoryServices.ActiveDirectory;
 
 namespace TurtleWPF.AppViewModel
 {
-    public class ViewModel: INotifyPropertyChanged
+    // Класс для хранения состояния черепашки
+    public class TurtleState
+    {
+        public string Coordinates { get; set; }
+        public string Angle { get; set; }
+        public string Color { get; set; }
+        public string PenCondition { get; set; }
+        public string Width { get; set; }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
     {
         private Turtle turtle;
+        private TurtleState _turtleState;
         private string _resultState;
+        private string _resultFigureState;
         private CommandInvoker invoker;
         private IDBAppReader dataBaseAppReader;
         private IDBAppWriter dataBaseAppWriter;
@@ -26,15 +33,14 @@ namespace TurtleWPF.AppViewModel
 
         public ViewModel(DBAppReader reader, DBAppWriter writer, CommandInvoker invoker)
         {
-            this.turtle = invoker.turtle; // Используем черепашку из инвокера
+            this.turtle = invoker.turtle; 
             this.invoker = invoker;
             dataBaseAppWriter = writer;
             dataBaseAppReader = reader;
             ExecuteCommand = new AsyncRelayCommand(SaveCommandAsync, CanExecuteCommand);
             checker = new NewFigureChecker(turtle, writer, reader);
-
-
         }
+
 
         public bool IsMoveChecked { get; set; }
         public bool IsAngleChecked { get; set; }
@@ -50,6 +56,16 @@ namespace TurtleWPF.AppViewModel
         public string ColorParameter { get; set; }
         public string WidthParameter { get; set; }
 
+        public TurtleState TurtleState
+        {
+            get => _turtleState;
+            set
+            {
+                _turtleState = value;
+                OnPropertyChanged();
+            }
+        }
+
         public string ResultState
         {
             get => _resultState;
@@ -60,18 +76,40 @@ namespace TurtleWPF.AppViewModel
             }
         }
 
+        public string ResultFigureState
+        {
+            get => _resultFigureState;
+            set
+            {
+                _resultFigureState = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ICommand ExecuteCommand { get; }
 
         private bool CanExecuteCommand(object parameter)
         {
-            return IsMoveChecked || 
-                IsAngleChecked || 
-                IsColorChecked || 
-                IsWidthChecked || 
-                IsPenUpChecked || 
+            return IsMoveChecked ||
+                IsAngleChecked ||
+                IsColorChecked ||
+                IsWidthChecked ||
+                IsPenUpChecked ||
                 IsPenDownChecked ||
                 IsHistoryChecked ||
                 IsListFiguresChecked;
+        }
+
+        private void UpdateTurtleState()
+        {
+            TurtleState = new TurtleState
+            {
+                Coordinates = $"Coords: ({turtle.GetCoordX()}; {turtle.GetCoordY()})",
+                Angle = $"Angle: {turtle.GetAngle()}",
+                Color = $"Color: {turtle.GetColor()}",
+                PenCondition = $"PenCondition: {turtle.GetPenCondition()}",
+                Width = $"Width: {turtle.GetWidth()}"
+            };
         }
 
         public async Task SaveCommandAsync(object parametr)
@@ -80,27 +118,23 @@ namespace TurtleWPF.AppViewModel
             {
                 if (IsHistoryChecked)
                 {
-                    // Вывод истории команд
                     var history = await dataBaseAppReader.GetCommands();
-                    ResultState = ""; // Очистить старые данные
-                    foreach (var comm in history)
-                    {
-                        ResultState += comm.CommandText + "\n"; // Добавить разделитель
-                    }
+                    TurtleState = null;
+                    ResultFigureState = null;
+                    string historyText = string.Join("\n", history.Select(comm => comm.CommandText));
+                    ResultState = historyText;
                 }
                 else if (IsListFiguresChecked)
                 {
-                    // Вывод списка фигур
+
                     var figures = await dataBaseAppReader.GetFigures();
-                    ResultState = ""; // Очистить старые данные
-                    foreach (var figure in figures)
-                    {
-                        ResultState += figure.FigureType + " " + figure.Parameters + "\n"; // Добавить разделитель
-                    }
+                    TurtleState = null;
+                    ResultState = null;
+                    string figuresText = string.Join("\n", figures.Select(figure => $"{figure.FigureType} {figure.Parameters}"));
+                    ResultFigureState = figuresText;
                 }
                 else
                 {
-                    // Выполнение остальных команд
                     if (IsMoveChecked)
                     {
                         invoker.Invoke(new MoveCommand(), MoveParameter);
@@ -132,24 +166,17 @@ namespace TurtleWPF.AppViewModel
                         await dataBaseAppWriter.SaveCommand("penUp");
                     }
 
-                    // Сохранение состояния черепашки
                     await dataBaseAppWriter.SaveTurtleStatus(turtle);
                     await checker.Check();
-
-                    // Обновление состояния черепашки
-                    ResultState = $"coords: ({turtle.GetCoordX()};{turtle.GetCoordY()})\n" +
-                                  $"angle: {turtle.GetAngle()}\n" +
-                                  $"color: {turtle.GetColor()}\n" +
-                                  $"penCondition: {turtle.GetPenCondition()}\n" +
-                                  $"width: {turtle.GetWidth()}";
+                    UpdateTurtleState();
                 }
             }
-
             catch (Exception ex)
             {
-                ResultState = "Error: " + ex.Message; // Выводить сообщение об ошибке
+                ResultState = "Error: " + ex.Message; 
             }
         }
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -157,9 +184,10 @@ namespace TurtleWPF.AppViewModel
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
     }
-    
-
 }
+
+
+
+
 
